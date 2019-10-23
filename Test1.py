51708144c708076c918629bfc3601d475bafc90c
ALockett89 <aklockett@fullsail.edu>
2019-10-22 23:38:47 -0700

    Plz tell me this works!!!

--- Test1.py
+++ Test1.py
@@ -1,1 +1,42 @@
-print("Bg twerk"),
+print("BYEAg twerk"),
+print(2+3), #Addition
+print(500-200), #Subtraction
+print(4*2), #Multiply
+print(9/3), #Divide(Float)
+print(12//3), #Divide(Interger)
+print(2**3), #Power Of(Instead of "2*2*2" use "X**")
+print(10%3), #Modulators(To get the remainder of 2 odd number when dividing)
+'Come get tht lil $40' #String
+'ballz'
+print('come get that lil $40'), #Printing a string
+print('come get tht lil "$40"'), #In oder to have qouats in a string, you must in with a single quaot(")
+print('Thats Renda\'s lil "$40"'), #Use (\) when trying to add possion in a string w/ & w/o ouats
+print('come get tht lil "$40"' + 'Thats Renda\'s lil "$40"'), #concagnate(Use (+) to add different strings together)
+print(10*'ballz'), #Numbers can be used to repeat a sting a number of times
+print('yeah with da\name'), #New Line(Use (\n) to creat a new line in your code)
+print(r'c:yea\ with da\ yea'), #Raw String(Use (r) the cancel special charictors)
+x = "$40" #Variable(Hold a value that can be called on later)
+
+print(x),
+x = 10
+y = 2
+print(x*y), #Variables can change & only the most recect 1 will count
+name = 'Renda'
+print("I love" + name),
+print(name + " is my wife!"),
+print(name[0]), #Variable Index(Use([] to fetch the character of a varible, they start with "0")
+print(name[-1]), #Use(-) to count the index backwords I.e instead of "1,2,3,4,5" it'll be "-5,-4,-3,-2,-1"
+print(name[0:3]), #Use(:) to show the indexed charater up until
+print(name[1:]), #If nothing is behind(:) then all thats indexed in that variable will be called/Works in reverse as well
+print(name[3:44]), #If the 2nd number exceeds the number of indexed characters then only those that are indexed will be shown, non will be added
+print(len(name)), #Use(Len) to count & show the number f charaters
+
+yea = [10,20,30,40,50,60,70,80,90,100]
+print(yea),
+print(yea[0]),
+
+
+
+
+
+
